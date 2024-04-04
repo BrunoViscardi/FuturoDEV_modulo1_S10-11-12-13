95a039b51e8761067368d82d879fdf157917a33e
Bruno Viscardi <bruno.viscardi33@gmail.com>
2024-04-04 11:37:05 -0300

    Criação de dados de trilha, desestruturação dos dados e chamada dos dados no card

--- Adventure-Trails-FD/src/App.jsx
+++ Adventure-Trails-FD/src/App.jsx
@@ -1,11 +1,32 @@
 import CardTrilha from "./components/cardTrilha.jsx";
 
-function App(){
+function App() {
 
-    return(
-        <div>
-            <CardTrilha/>
-        </div>
+    const listaTrilhas = [
+        {
+            nomeTrilha: "Trilha da Costa da Lagoa",
+            cidade: "Florianópolis",
+            estado: "SC",
+            duracao: "120",
+            trajeto: "4",
+            dificuldade: "iniciante",
+            tipo: "caminhada / trekking",
+            nomeUsuario: "Bruno Viscardi",
+            urlImagem: "https://images.pexels.com/photos/17951453/pexels-photo-17951453/free-photo-of-homem-caminhando-andando-floresta.jpeg"
+        }
+    ]
+
+    return (
+        <>
+
+            {
+                listaTrilhas.map((trilha, index) => (
+                    <CardTrilha dadosTrilha={trilha} key={index} />
+
+                ))
+            }
+
+        </>
     )
 }
 
--- Adventure-Trails-FD/src/components/cardTrilha.jsx
+++ Adventure-Trails-FD/src/components/cardTrilha.jsx
@@ -1,10 +1,17 @@
-function CardTrilha(){
+function CardTrilha({dadosTrilha}){    
 
     return(
-        <div>
-            <h1>Card Trilha</h1>
-        </div>
+        <>
+            <img width={200} src={dadosTrilha.urlImagem} alt="imagem trilha" />
+            <h1>{dadosTrilha.nomeTrilha} - {dadosTrilha.cidade} / {dadosTrilha.estado}</h1>
+            <span>Por: {dadosTrilha.nomeUsuario}</span>
+            <span>Duração: {dadosTrilha.duracao} min </span>
+            <span>Trajeto: {dadosTrilha.trajeto} km</span>
+            <span>{dadosTrilha.dificuldade}</span>
+        </>
     )
+
+    
 }
 
 export default CardTrilha
