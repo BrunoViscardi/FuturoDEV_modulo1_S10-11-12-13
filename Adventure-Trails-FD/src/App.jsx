b7fc019f7bd72f2f392b50dfed629971d12ba54c
Bruno Viscardi <bruno.viscardi33@gmail.com>
2024-04-05 16:12:50 -0300

    concertando a desestruturação

--- Adventure-Trails-FD/src/App.jsx
+++ Adventure-Trails-FD/src/App.jsx
@@ -4,28 +4,24 @@ function App() {
 
     const listaTrilhas = [
         {
-            nomeTrilha: "Trilha da Costa da Lagoa",
+            nomeTrilha: "Thilha da Costa da Lagoa",
             cidade: "Florianópolis",
             estado: "SC",
             duracao: 120,
             trajeto: 4,
             dificuldade: "iniciante",
-            tipo: "caminhada / trekking",
-            nomeUsuario: "Bruno Viscardi",
-            urlImagem: "https://images.pexels.com/photos/17951453/pexels-photo-17951453/free-photo-of-homem-caminhando-andando-floresta.jpeg"
+            nomeUsuario: "caminhada",
+            urlImagem: "https://images.pexels.com/photos/917510/pexels-photo-917510.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         }
     ]
 
     return (
         <>
-
             {
                 listaTrilhas.map((trilha, index) => (
-                    <CardTrilha dadosTrilha={trilha} key={index} />
-
-                ))
+                <CardTrilha dadosTrilha={trilha} key={index} />
+            ))
             }
-
         </>
     )
 }
