60431d00e29d91466c1e7dc70e31b5237337c3f3
Bruno Viscardi <bruno.viscardi33@gmail.com>
2024-04-12 09:48:00 -0300

    criando funcao de gerenciamento de rotas

--- Adventure-Trails-FD/src/main.jsx
+++ Adventure-Trails-FD/src/main.jsx
@@ -2,9 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+
+const routers = createBrowserRouter([
+  {
+    path:"/",
+    element:
+  },
+  {
+    path:"/cadastro",
+    element:
+  },
+  {
+    path:"/lista",
+    element:
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={routers}>
+  </RouterProvider>
 )
