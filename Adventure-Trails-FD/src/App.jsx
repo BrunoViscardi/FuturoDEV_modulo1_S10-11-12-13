cb56483945debdb2beb675a55d3ded612622b99b
Bruno Viscardi <bruno.viscardi33@gmail.com>
2024-04-12 15:33:56 -0300

    código do app na pag trilhas

--- Adventure-Trails-FD/src/App.jsx
+++ Adventure-Trails-FD/src/App.jsx
@@ -1,46 +1,10 @@
-import CardTrilha from "./components/cardTrilha/index.jsx";
 import "./App.css"
-import useFetch from "./Hooks/useFetch.js";
-import Header from "./components/header/index.jsx";
+
 
 function App() {
-
-    const [data, isLoading] = useFetch("/dadosTrilha.json");
-    
-
-    if (isLoading || !data) {
-        return (        
-            <div className="cardContainer">
-                Carregando...
-            </div>
-        )
-    }    
-
-    return (
+    return(
         <>
-        <Header/>
-
-
-        <div className="Retangulo"> <div></div></div>
-        
-        
-
-
-
-        <div className="cardContainer">
-            <div className="titulo-cardContainer">
-                Explore trilhas incríveis
-            </div>
-
-
-            {
-                data.map((trilha, index) => (
-                <CardTrilha dadosTrilha={trilha} key={index} />
-            ))
-            }
-        </div>
         </>
-    )
+    )    
 }
-
 export default App
--- Adventure-Trails-FD/src/pages/trilhas/trilhas.jsx
+++ Adventure-Trails-FD/src/pages/trilhas/trilhas.jsx
@@ -1,8 +1,44 @@
-function Trilhas(){
+import CardTrilha from "../../components/cardTrilha/index.jsx";
+import useFetch from "../../Hooks/useFetch.js";
+import Header from "../../components/header/index.jsx";
+import './trilhas.css'
 
-    return(
+
+function Trilhas(){
+    const [data, isLoading] = useFetch("/dadosTrilha.json");
+    
+
+    if (isLoading || !data) {
+        return (        
+            <div className="cardContainer">
+                Carregando...
+            </div>
+        )
+    }    
+
+    return (
         <>
-        <h1>Página trilhas</h1>
+        <Header/>
+
+
+        <div className="Retangulo"> <div></div></div>
+        
+        
+
+
+
+        <div className="cardContainer">
+            <div className="titulo-cardContainer">
+                Explore trilhas incríveis
+            </div>
+
+
+            {
+                data.map((trilha, index) => (
+                <CardTrilha dadosTrilha={trilha} key={index} />
+            ))
+            }
+        </div>
         </>
     )
 }
