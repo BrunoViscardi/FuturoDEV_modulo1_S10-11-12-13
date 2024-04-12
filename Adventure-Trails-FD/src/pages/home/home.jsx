cdbc71db3a6eacfcd0f71eebfa8e37206080ffb3
Bruno Viscardi <bruno.viscardi33@gmail.com>
2024-04-12 15:13:26 -0300

    estilizacao sections

--- Adventure-Trails-FD/src/pages/home/home.jsx
+++ Adventure-Trails-FD/src/pages/home/home.jsx
@@ -24,9 +24,9 @@ function Home() {
             </section>
 
 
-            <section>
-                <h1> Explore trilhas incríveis </h1>
-                <p> O "Adventure Trails FD" é seu portal para explorar e compartilhar as melhores trilhas para trekking e ciclismo ao redor do mundo. Descubra rotas deslumbrantes, desde caminhos tranquilos por florestas exuberantes até trilhas desafiadoras em montanhas majestosas. Encontre informações detalhadas sobre cada trilha, incluindo distância, dificuldade, pontos de interesse naturais e dicas úteis para uma experiência eco-friendly.</p>
+            <section className="explorarContainer">
+                <h1 className="tituloSection"> Explore trilhas incríveis </h1>
+                <p className="textoSection"> O "Adventure Trails FD" é seu portal para explorar e compartilhar as melhores trilhas para trekking e ciclismo ao redor do mundo. Descubra rotas deslumbrantes, desde caminhos tranquilos por florestas exuberantes até trilhas desafiadoras em montanhas majestosas. Encontre informações detalhadas sobre cada trilha, incluindo distância, dificuldade, pontos de interesse naturais e dicas úteis para uma experiência eco-friendly.</p>
                 <div>
                     <Link to="/trilhas">
                         <CButton>Explore trilhas</CButton>
@@ -35,19 +35,15 @@ function Home() {
             </section>
 
 
-            <section>
+            <section className="cadastrarContainer">
                 <div>
-                    <h1> Compartilhe fotos, dicas e localização das suas trilhas favoritas</h1>
+                    <h1 className="tituloSection"> Compartilhe fotos, dicas e localização das suas trilhas favoritas</h1>
                     <Link to="/cadastro">
                         <CButton>Cadastrar nova trilha</CButton>
                     </Link>
                 </div>
-                <img src="/footpath" alt="caminho na floresta" />
+                <img src="/footpath.jpg" alt="caminho na floresta" />
             </section>
-
-
-
-
         </>
     )
 }
