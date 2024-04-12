890d383962cc5be36d017f1c494cdaf787968035
Bruno Viscardi <bruno.viscardi33@gmail.com>
2024-04-12 11:33:32 -0300

    criacao do header e retangulo

--- Adventure-Trails-FD/src/pages/Home.jsx
+++ Adventure-Trails-FD/src/pages/Home.jsx
@@ -1,8 +1,27 @@
-function Home(){
+import Header from "../components/header";
+import './home.css'
 
-    return(
+
+function Home() {
+
+    return (
         <>
-        <h1>Página Home</h1>
+            <Header />
+
+            <div className="RetanguloHome">
+                <div className="RetanguloContainer">
+                    <div className="RetanguloConteudo">
+                        <h1> Que tal aproveitar um tempo <br/>
+                            com a natureza?</h1>                        
+                        <p> Junte-se à comunidade de entusiastas ao ar livre, compartilhe suas aventuras e inspire-se com as experiências de outros aventureiros. Prepare-se para explorar novos horizontes e se conectar com a natureza através do Adventure Trails!</p>
+                        <button> Explorar Trilhas </button>
+                    </div>
+                </div>
+            </div>
+
+
+
+
         </>
     )
 }
