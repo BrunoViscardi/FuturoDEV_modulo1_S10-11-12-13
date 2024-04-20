bc364070cb8413340762debdd74590273b459ff7
Bruno Viscardi <bruno.viscardi33@gmail.com>
2024-04-20 16:16:31 -0300

    refatorando função de apresentação dos cards Trilhas

--- Adventure-Trails-FD/src/pages/trilhas/trilhas.jsx
+++ Adventure-Trails-FD/src/pages/trilhas/trilhas.jsx
@@ -3,41 +3,27 @@ import useFetch from "../../Hooks/useFetch.js";
 import './trilhas.css'
 
 
-function Trilhas(){
+function Trilhas() {
     const [data, isLoading] = useFetch("/dadosTrilha.json");
-    
 
-    if (isLoading || !data) {
-        return (        
-            <div className="cardContainer">
-                Carregando...
-            </div>
-        )
-    }    
 
     return (
         <>
-        
+
+            <div className="Retangulo"> <div></div></div>
+
+            <div className="cardContainer">
+                <div className="titulo-cardContainer">
+                    Explore trilhas incríveis
+                </div>
 
 
-        <div className="Retangulo"> <div></div></div>
-        
-        
-
-
-
-        <div className="cardContainer">
-            <div className="titulo-cardContainer">
-                Explore trilhas incríveis
+                {Array.isArray(data) && !isLoading ? (
+                    data.map((trilha, index) => (
+                        <CardTrilha dadosTrilha={trilha} key={index} />))
+                ) : (<span> Não há dados disponíveis</span>)
+                }
             </div>
-
-
-            {
-                data.map((trilha, index) => (
-                <CardTrilha dadosTrilha={trilha} key={index} />
-            ))
-            }
-        </div>
         </>
     )
 }
